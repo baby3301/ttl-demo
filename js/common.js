b8f71d7ea981bf7eb88f717600ea45514047fc66
Hoang Nguyen <hoang.nguyen@urekamedia.vn>
2026-07-30 15:55:42 +0700

    update ver js

--- js/common.js
+++ js/common.js
@@ -67,43 +67,6 @@ function exportImage() {
    }
 }
 
-
-
-function exportDomToImg() {
-   if (onRun) return;
-   if (onExport) return;
-
-   const inputValue = document.getElementById("search").value || "screenshot";
-   const element = document.body;
-   if (!element) return;
-
-   const actionElement = document.getElementById('actions');
-   if (actionElement) {
-      actionElement.style.opacity = 0;
-   }
-
-   domtoimage.toPng(element, {
-      // filter: (node) => node.id !== 'actions'
-      width: element.clientWidth * 2, // Tăng độ nét (scale 2x)
-      height: element.clientHeight * 2,
-      style: {
-         transform: 'scale(2)',
-         transformOrigin: 'top left'
-      }
-   }).then(function (dataUrl) {
-      const link = document.createElement('a');
-      link.download = `${inputValue}-${Date.now()}.png`;
-      link.href = dataUrl;
-      link.click();
-   }).catch(function (error) {
-      console.error('Lỗi khi tạo ảnh:', error);
-   }).finally(function () {
-      if (actionElement) {
-         actionElement.style.opacity = 1; 
-      }
-   });
-}
-
 function exportHtmlToImg() {
    if (onRun) return;
    if (onExport) return;
